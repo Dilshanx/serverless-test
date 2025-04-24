9f88375f68aa8e401abb377c9356f9c752c33a18
Dilshanx <dilshanjayatissa@gmail.com>
2025-04-24 20:31:51 +0530

    ewewewe

--- routes/testRoutes2.js
+++ routes/testRoutes2.js
@@ -3,7 +3,7 @@ import routes from "express";
 const router = routes.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World from test 2");
+  res.send("Hello World from test 2 deerer");
 });
 
 export default router;
