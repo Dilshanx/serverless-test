6e1f1e387c847a2071276c13b498b30b3f90500c
Dilshanx <dilshanjayatissa@gmail.com>
2025-04-24 20:46:27 +0530

    sucesse commit

--- routes/testRoutes2.js
+++ routes/testRoutes2.js
@@ -3,7 +3,7 @@ import routes from "express";
 const router = routes.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World from test 2 deerer");
+  res.send("Hello World from test 2 dilshan");
 });
 
 export default router;
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World dilshan!");
 });
 
 app.use("/test/one", testRoutes1);
