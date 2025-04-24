0dc500b84dcfc5c4e89464fae090633966bf745a
Dilshanx <dilshanjayatissa@gmail.com>
2025-04-24 20:11:53 +0530

    khkjk

--- routes/testRoutes1.js
+++ routes/testRoutes1.js
@@ -3,7 +3,7 @@ import routes from "express";
 const router = routes.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World  from test 1 suc");
+  res.send("Hello World  from test 1 suchkhk");
 });
 
 export default router;
